146386f2c54fd42eaa2ba3a384c4b513b3242b0b
caprabell <mauriciocbell@gmail.com>
2018-10-16 11:22:18 -0300

    Cleaning code

--- src/app.js
+++ src/app.js
@@ -2,20 +2,20 @@
 // * carrega tudo da lib
 // as ==> alias para o método
 
-//import * as R from 'ramda';
+// import * as R from 'ramda';
 import { union as junta, uniq } from 'ramda';
 
-import sum from './utils.js';
+import sum from './utils';
 
-const arr1 = [1,1,1,2,2,3,4,5,6,6];
-const arr2 = [5,6,6,6,7,7,8,9,10];
+const arr1 = [1, 1, 1, 2, 2, 3, 4, 5, 6, 6];
+const arr2 = [5, 6, 6, 6, 7, 7, 8, 9, 10];
 
-const arr3 =  junta(arr1,arr2);
+const arr3 = junta(arr1, arr2);
 
-const arr4 =  uniq(arr2);
+const arr4 = uniq(arr2);
 
 console.log(arr3);
 
 console.log(arr4);
 
-console.log(sum(5,6));
+console.log(sum(5, 6));
--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,9 @@
-function sum(a,b) {
+function sum(a, b) {
   return a + b;
 }
 
-//método principal
-//somente um default por arquivo
-//importar com qualquer nome
-//não precisa usar chaves
- export default sum;
+// método principal
+// somente um default por arquivo
+// importar com qualquer nome
+// não precisa usar chaves
+export default sum;
